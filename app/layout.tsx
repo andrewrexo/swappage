cbd9582c130efe5b6bb3cfd52a6b93c514b8a700
Andrew R <beta@runelite.bot>
2024-08-11 20:21:13 -0500

    feat: add radix theme to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import '@radix-ui/themes/styles.css';
+import { Theme, ThemePanel } from '@radix-ui/themes';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,7 +18,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Theme
+          accentColor="crimson"
+          grayColor="sand"
+          radius="large"
+          scaling="95%"
+        >
+          {children}
+          <ThemePanel />
+        </Theme>
+      </body>
     </html>
   );
 }
