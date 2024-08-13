3d0b28f42380381452ebd629ab959ab7b6fe787a
Andrew R <beta@runelite.bot>
2024-08-13 12:06:18 -0500

    feat: hide overflow from top movers

--- app/widgets/swap/components/asset-dialog/top-movers.tsx
+++ app/widgets/swap/components/asset-dialog/top-movers.tsx
@@ -1,12 +1,5 @@
-import { RadioCards, Flex, Text, Box, Heading } from '@radix-ui/themes';
-import {
-  ArrowDownCircle,
-  ArrowLeft,
-  ArrowRight,
-  ArrowUp01Icon,
-  ArrowUpCircle,
-  CircleArrowRightIcon,
-} from 'lucide-react';
+import { RadioCards, Flex, Text, Box } from '@radix-ui/themes';
+import { ArrowUpCircle } from 'lucide-react';
 
 export function TopMovers() {
   return (
@@ -14,8 +7,13 @@ export function TopMovers() {
       <Text size="1" color="gray">
         Moving fast
       </Text>
-      <Flex justify="between" gap="4" align="center" className="py-2 pb-0">
-        <RadioCards.Root className="flex w-full justify-between overflow-x-scroll">
+      <Flex
+        justify="between"
+        gap="4"
+        align="center"
+        className="overflow-hidden py-2 pb-0"
+      >
+        <RadioCards.Root className="flex w-full justify-between">
           <RadioCards.Item value="1" className="w-full">
             <Flex direction="column" className="w-full">
               <Text weight="bold" className="text-accent">
