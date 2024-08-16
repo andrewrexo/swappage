b9ef61d1b68a4bf5276d029b3a0bef762dfdf36e
Andrew R <beta@runelite.bot>
2024-08-16 12:33:17 -0500

    feat: add routes to fetch rates

---
+++ app/api/rates/[pair_id]/route.ts
@@ -0,0 +1,23 @@
+import { getRateByPairId } from '@/lib/widgets/swap/lib/rates';
+import { NextResponse } from 'next/server';
+import { NextRequest } from 'next/server';
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { pair_id: string } },
+) {
+  const pairId = params.pair_id;
+
+  if (!pairId) {
+    return NextResponse.json({ error: 'Pair ID is required' }, { status: 400 });
+  }
+
+  const userProperties = {
+    ip: req.ip || '',
+    device: req.headers.get('user-agent') || '',
+  };
+
+  const rates = await getRateByPairId(pairId, userProperties.ip);
+
+  return NextResponse.json(rates);
+}
---
+++ lib/widgets/swap/lib/rates.ts
@@ -0,0 +1,28 @@
+import { swapApiUrl } from '@/lib/util';
+import { PairRate } from './exodus/rate';
+
+export const getRateByPairId = async (pairId: string, ip: string) => {
+  console.log('getRateByPairId', pairId, ip);
+  if (!pairId.includes('_')) {
+    throw new Error(
+      'Invalid pairId, pairId must be in the format of assetId_assetId',
+    );
+  }
+
+  const requestEndpoint = `${swapApiUrl}/rates/${pairId}`;
+  const response = await fetch(requestEndpoint);
+
+  if (!response.ok) {
+    throw new Error(
+      `Failed to fetch rate for pairId: ${pairId}. ${response.url}`,
+    );
+  }
+
+  const data: PairRate[] = await response.json();
+
+  if (Array.isArray(data)) {
+    return data[0];
+  }
+
+  return data;
+};
