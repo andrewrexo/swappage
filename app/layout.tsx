005d5401b1d7a70f8306a256fef0f31e215d0a6d
Andrew R <beta@runelite.bot>
2024-08-19 00:43:10 -0500

    feat: revert back to dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Theme
-          appearance="light"
+          appearance="dark"
           accentColor="mint"
           grayColor="mauve"
           radius="large"
