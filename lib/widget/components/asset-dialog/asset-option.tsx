d58736437e43422f5e7723f1678299d34f065d39
Andrew R <beta@runelite.bot>
2024-08-22 01:36:44 -0500

    feat: add token icons to asset selection

--- lib/widget/components/asset-dialog/asset-option.tsx
+++ lib/widget/components/asset-dialog/asset-option.tsx
@@ -2,6 +2,14 @@ import { Flex, Text, Badge } from '@radix-ui/themes';
 import { ExodusAsset } from '../../lib/exodus/asset';
 import networkToColor from '../../features/assets/color';
 import { twMerge } from 'tailwind-merge';
+import { NetworkIcon, TokenIcon } from '@web3icons/react';
+
+const networkToIcon = {
+  ethereum: 'ethereum',
+  arbitrumone: 'arbitrum-one',
+  basemainnet: 'base',
+  solana: 'solana',
+};
 
 export function AssetOption({
   asset,
@@ -10,12 +18,28 @@ export function AssetOption({
   asset: ExodusAsset;
   onAssetSelect: (asset: ExodusAsset) => void;
 }) {
+  const iconId = (() => {
+    const symbolLower = asset.symbol.toLowerCase();
+    const knownTokens = {
+      usdt: 'USDT',
+      usdc: 'USDC',
+    };
+
+    for (const [token, id] of Object.entries(knownTokens)) {
+      if (symbolLower.includes(token)) {
+        return id;
+      }
+    }
+
+    return asset.symbol;
+  })();
+
   return (
     <Flex
       className={twMerge(
         'flex w-full cursor-pointer rounded-lg',
-        'px-1 py-3 sm:px-1',
-        'bg-gradient-to-r from-[var(--gray-1)] to-[var(--gray-3)]',
+        'px-2 py-4 sm:px-2',
+        'bg-gradient-to-r from-[var(--gray-2)] to-[var(--gray-4)]',
         'transition-all duration-300',
       )}
       onClick={() => onAssetSelect(asset)}
@@ -23,6 +47,20 @@ export function AssetOption({
     >
       <Text as="div" color="gray">
         <Flex align="center" gap="2">
+          <TokenIcon
+            symbol={iconId}
+            size={32}
+            variant="branded"
+            fallback={
+              <NetworkIcon
+                network={
+                  networkToIcon[asset.network as keyof typeof networkToIcon]
+                }
+                variant="branded"
+                size={32}
+              />
+            }
+          />
           <Text
             weight="bold"
             size="4"
