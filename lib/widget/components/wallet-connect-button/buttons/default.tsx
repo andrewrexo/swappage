52c911503bf38a10d14c55208705fe1945d01628
Andrew R <beta@runelite.bot>
2024-08-23 16:12:20 -0500

    feat: silence react ref error

--- lib/widget/components/wallet-connect-button/buttons/default.tsx
+++ lib/widget/components/wallet-connect-button/buttons/default.tsx
@@ -1,34 +1,39 @@
+import React, { forwardRef } from 'react';
 import { type ButtonProps, Button } from '@radix-ui/themes';
 import { motion, type MotionProps } from 'framer-motion';
-import type { ReactNode } from 'react';
+import type { ReactNode, ComponentProps } from 'react';
 
 const MotionButton = motion(Button);
 
-export const DefaultButton = ({
-  children,
-  onClick,
-  accountOnly = false,
-  ...props
-}: {
+type MotionButtonProps = ComponentProps<typeof MotionButton>;
+
+interface DefaultButtonProps
+  extends Omit<MotionButtonProps, 'onAnimationStart'> {
+  onAnimationStart?: MotionButtonProps['onAnimationStart'];
   children: ReactNode;
   onClick?: () => void;
   accountOnly?: boolean;
-} & ButtonProps &
-  MotionProps) => {
-  return (
-    <MotionButton
-      variant={accountOnly ? 'surface' : 'surface'}
-      asChild
-      className="flex cursor-pointer"
-      onClick={onClick}
-      name="connect-wallet"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 0.2 }}
-      {...(!accountOnly ? { whileHover: { scale: 1.02 } } : {})}
-      {...props}
-    >
-      <motion.button>{children}</motion.button>
-    </MotionButton>
-  );
-};
+}
+
+export const DefaultButton = forwardRef<HTMLButtonElement, DefaultButtonProps>(
+  ({ children, onClick, accountOnly = false, ...props }, ref) => {
+    return (
+      <MotionButton
+        variant={accountOnly ? 'surface' : 'surface'}
+        className="flex cursor-pointer"
+        onClick={onClick}
+        name="connect-wallet"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.2 }}
+        {...(!accountOnly ? { whileHover: { scale: 1.02 } } : {})}
+        {...props}
+        ref={ref}
+      >
+        {children}
+      </MotionButton>
+    );
+  },
+);
+
+DefaultButton.displayName = 'Button';
